dd95438df999605661861448f3445fc1f020b1e1
Shranobxtha <gb11shranob@gmail.com>
2026-07-08 09:41:32 +0545

    added react

---
+++ todo-app/src/App.jsx
@@ -0,0 +1,17 @@
+function PageTitle(){
+  return(
+    <>
+      <h1>My Todo List Application</h1>
+    </>
+  );
+}
+
+function App() {
+  return (
+    <>
+      <PageTitle/>
+    </>
+  );
+}
+
+export default App;
