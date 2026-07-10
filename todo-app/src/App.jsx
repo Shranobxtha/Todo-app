99abbc04cea60c3b7b939ab65017aa786b601235
Shranobxtha <90218115+Shranobxtha@users.noreply.github.com>
2026-07-10 09:46:20 +0545

    changes

--- todo-app/src/App.jsx
+++ todo-app/src/App.jsx
@@ -1,19 +1,34 @@
-import PageTitle from './components/PageTitle/PageTitle';
+import { useState, useEffect } from "react";
+import PageTitle from "./components/PageTitle/PageTitle";
 import TaskContainer from "./components/TaskContainer/TaskContainer";
+import Form from "./components/Form/Form";
+import PageHeader from "./components/PageHeader/PageHeader";
+import { getTasks } from "./services/TaskService";
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+  const [currentPage, setCurrentPage] = useState(0);
   const containerTitle = "Tasks Due Today";
-  const tasks = [
-  { time: "9:00 AM", text: "Get eggs", isUrgent: true },
-  { time: "9:05 AM", text: "Clean your room", isUrgent: false },
-  { time: "10:00 AM", text: "Complete task1", isUrgent: true },
-  { time: "4:00 PM", text: "Go for a walk", isUrgent: false },
-  ];
+
+  useEffect(() => {
+    const loadTasks = async () => {
+      let savedTasks = await getTasks();
+      setTasks(savedTasks);
+    };
+
+    if (currentPage === 0) {
+        loadTasks();
+    }
+  }, [currentPage]);
 
   return (
     <>
-      <PageTitle/>
-      <TaskContainer containerTitle={containerTitle} tasks={tasks} />
+      <PageTitle />
+      <PageHeader currentPage={currentPage} setCurrentPage={setCurrentPage} />
+      {currentPage === 1 && <Form setCurrentPage={setCurrentPage} />}
+      {currentPage === 0 && (
+        <TaskContainer containerTitle={containerTitle} tasks={tasks} />
+      )}
     </>
   );
 }
